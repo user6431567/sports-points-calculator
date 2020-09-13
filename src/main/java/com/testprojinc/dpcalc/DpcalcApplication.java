4cb0ab6d40a8ff6818ed367150dd5d053058a62b
Aleksandr Gavrikov <>
2020-09-13 15:47:04 +0300

    SpringBoot application created

---
+++ src/main/java/com/testprojinc/dpcalc/DpcalcApplication.java
@@ -0,0 +1,13 @@
+package com.testprojinc.dpcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpcalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testprojinc/dpcalc/DpcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testprojinc.dpcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
